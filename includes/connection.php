4161441a942f0cad3362475387442c48d25f6fba
anupam-tiwari <40127722+anupam-tiwari@users.noreply.github.com>
2021-04-24 04:08:39 +0530

    Add files via upload

---
+++ includes/connection.php
@@ -0,0 +1,18 @@
+<?php
+$con = mysqli_connect("localhost","root","","social_network");
+//$con = mysqli_connect("remotemysql.com" ,"Ncmv0vMxAr" ,"Xgee11P1le" ,"Ncmv0vMxAr");
+if(!$con){
+    echo "Not connected";
+}else{
+    echo "connected";
+
+}
+ //   $server = "sql303.epizy.com";
+//    $username = "epiz_28405349";
+//    $password = "ibwQOhtQsoW5V";
+//    $dbname = "epiz_28405349_wap";
+
+//$con = mysqli_connect($server, $username, $password, $dbname );
+?>
+
+
